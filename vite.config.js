70b13715213c713d50a75f25bd633d4f33247712
Joan Narvaez <97415410+jimjoa29@users.noreply.github.com>
2026-05-03 02:50:39 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/react-swc' // O el plugin que estés usando
 
 export default defineConfig({
-    plugins: [react()],
+  plugins: [react()],
+  server: {
+    allowedHosts: [
+      'devserver-main--control-de-sacos-modificados.netlify.app',
+      'localhost'
+    ]
+  }
 })
